12dbbab0cd6509b5399d8f732ad899edcd214042
Navashub <navasmuller01@gmail.com>
2024-03-20 18:39:55 +0300

    done with the collections components

---
+++ react-eccomerce/src/pages/home/BestSellers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BestSellers = () => {
+  return (
+    <div className='max-w-screen-2xl container mx-auto xl:px-28 px-4'>BestSellers</div>
+  )
+}
+
+export default BestSellers
--- react-eccomerce/src/pages/home/Products.jsx
+++ react-eccomerce/src/pages/home/Products.jsx
@@ -38,6 +38,35 @@ const Products = () => {
         setFilteredItems(products);
         selectedCategory("all");
     }
+
+    //sorting functions
+    const handleSortChange = (option) => {
+        setSortOption(option);
+
+        //logic for the sort
+        let sortedItems = [...filteredItems];
+        
+        switch (option) {
+            case "A-Z":
+                sortedItems.sort((a,b) => a.title.localeCompare(b.title));
+                break;
+            case "Z-A":
+                sortedItems.sort((a,b) => b.title.localeCompare(a.title));
+                break;
+            case "low-to-high":
+                sortedItems.sort((a,b) => a.price - b.price);
+                break;
+            case "high-to-low":
+                sortedItems.sort((a,b) => b.price - a.price);
+                break;
+            default:
+                break;
+        }
+
+        setFilteredItems(sortedItems);
+    }
+    
+
     return (
         <div className='max-w-screen-2xl container mx-auto xl:px-28 px-4 mb-12'>
             <h2 className='text-3xl font-semibold capitalize text-center my-8'>Or subscribe to the newsletter</h2>
@@ -58,7 +87,11 @@ const Products = () => {
                         <div className='bg-black p-2'>
                             <FaFilter className='text-white h-4 w-4' />
                         </div>
-                        <select className='bg-black text-white px-2 py-1 rounded-sm'>
+                        <select 
+                        id='sort'
+                        onChange={(e) => handleSortChange(e.target.value)}
+                        value={sortOption}
+                        className='bg-black text-white px-2 py-1 rounded-sm'>
                             <option value="Default">Default</option>
                             <option value="A-Z">A-Z</option>
                             <option value="Z-A">Z-A</option>
